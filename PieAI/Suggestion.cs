79b5c74e380b009a0a7dd9e430e1b3408b9a003e
Rafael Santos <rafaelfsantos03@gmail.com>
2025-09-21 02:07:38 +0100

    ChatBot implementation + partial metrics review

---
+++ PieAI/Suggestion.cs
@@ -0,0 +1,9 @@
+﻿namespace PieAI
+{
+    public class Suggestion
+    {
+        public string Name { get; set; }
+        public string Current { get; set; }
+        public string Recomended { get; set; }
+    }
+}
